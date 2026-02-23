637a91cd81ffed13cfed8a08ae664f7ac2e13015
erilsion <erilsion@gmail.com>
2026-02-23 13:20:05 +0900

    Feat: 플레이어 이동 및 점프 구현

--- Assets/02.Scripts/Player/PlayerMoveAbility.cs
+++ Assets/02.Scripts/Player/PlayerMoveAbility.cs
@@ -1,16 +1,38 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMoveAbility : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _moveSpeed = 7f;
+    [SerializeField] private float _jumpForce = 2.5f;
+    private const float GRAVITY = 9.8f;
+    private float _yVelocity = 0f;
+
+    private CharacterController _characterController;
+
+    // 1. 중력 적용하기.
+    // 2. 스페이스바 누르면 점프하기.
+
+    private void Awake()
     {
-        
+        _characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(h, 0, v);
+        direction.Normalize();
+
+        _yVelocity -= GRAVITY * Time.deltaTime;
+        direction.y = _yVelocity;
+
+        if (Input.GetKey(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _yVelocity = _jumpForce;
+        }
+
+        _characterController.Move(direction * _moveSpeed * Time.deltaTime);
     }
 }
