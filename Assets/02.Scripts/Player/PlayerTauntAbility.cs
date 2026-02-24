c8fdaaa045a469ca23d04d9711d1ba2faf1dcc56
erilsion <erilsion@gmail.com>
2026-02-24 13:30:13 +0900

    Feat: 플레이어 도발 관련 클래스 생성

---
+++ Assets/02.Scripts/Player/PlayerTauntAbility.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerTauntAbility : PlayerAbility
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            _animator.Play("Taunt1");
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            _animator.Play("Taunt2");
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            _animator.Play("Taunt3");
+        }
+    }
+}
