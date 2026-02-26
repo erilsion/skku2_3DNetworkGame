3a82afb260d43647fab69ca128d55b9a97aaac1a
erilsion <erilsion@gmail.com>
2026-02-26 13:40:55 +0900

    Feat: 점수 오브젝트가 하늘에서 랜덤 스폰 구현

---
+++ Assets/02.Scripts/Item/ItemSpawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public static ItemSpawner Instance;
+
+    [SerializeField] private float _minSpawnPosition = -30f;
+    [SerializeField] private float _maxSpawnPosition = 30f;
+    [SerializeField] private float _ySpawnPosition = 8f;
+
+    [SerializeField] private float _itemRespawnCooltime = 5f;
+    private float _itemRespawnTimer = 0f;
+
+    private void Awake()
+    {
+        Instance = this;
+        _itemRespawnTimer = 0f;
+    }
+
+    private void Update()
+    {
+        _itemRespawnTimer += Time.deltaTime;
+        if (_itemRespawnTimer >= _itemRespawnCooltime)
+        {
+            ItemSpawn();
+            _itemRespawnTimer = 0f;
+        }
+    }
+
+    public void ItemSpawn()
+    {
+        transform.position = GetRandomSpawnPoint();
+        ItemObjectFactory.Instance.RequestMakeScoreItem(transform.position);
+    }
+
+    public Vector3 GetRandomSpawnPoint()
+    {
+        float xSpawnPosition = Random.Range(_minSpawnPosition, _maxSpawnPosition);
+        float zSpawnPosition = Random.Range(_minSpawnPosition, _maxSpawnPosition);
+        Vector3 spawnPoint = new Vector3(xSpawnPosition, _ySpawnPosition, zSpawnPosition);
+        return spawnPoint;
+    }
+}
