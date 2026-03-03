f4711008b4f8ed044ca657a6db57a2cd7099130d
erilsion <erilsion@gmail.com>
2026-03-03 14:49:07 +0900

    Feat: Linq를 이용하여 Score UI 구현

--- Assets/02.Scripts/UI/ScoreItemUI.cs
+++ Assets/02.Scripts/UI/ScoreItemUI.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 public class ScoreItemUI : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _rankTextUI;
     [SerializeField] private TextMeshProUGUI _nicknameTextUI;
     [SerializeField] private TextMeshProUGUI _scoreTextUI;
 
-    public void Set(string nickname, int score)
+    public void Set(int rank, string nickname, int score)
     {
+        _rankTextUI.text = $"{rank}";
         _nicknameTextUI.text = nickname;
         _scoreTextUI.text = $"{score:N0}";
     }
--- Assets/02.Scripts/UI/ScoreUI.cs
+++ Assets/02.Scripts/UI/ScoreUI.cs
@@ -1,5 +1,6 @@
-﻿using System.Linq;
+﻿using ExitGames.Client.Photon.StructWrapping;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ScoreUI : MonoBehaviour
@@ -19,7 +20,6 @@ public class ScoreUI : MonoBehaviour
         var scores = ScoreManager.Instance.Scores;
 
         // ReadOnly가 아니면 원본을 수정하게 되므로 무결성에 문제가 생긴다.
-        
         List<ScoreData> scoreDatas = scores.Values.ToList();
 
         // 1. todo: 1등부터 3등까지 정렬한다. 3명이 있는 지 적절하게 반복문을 진행한다.
@@ -27,10 +27,16 @@ public class ScoreUI : MonoBehaviour
         //          - 정리 과제: Linq를 사용한다. (무엇인지, 언제 쓰이는 지, 장단점은 무엇인지)
         // 2. todo: 점수 1,000점마다 플레이어 무기의 scale이 0.1씩 증가한다. (동기화 되어야 한다.)
         // 3. todo: 죽으면 점수의 반을 잃는다.
-        for (int i = 0; i < _items.Count; i++)
+
+        // 점수가 높은 사람이 1등이라 Descending(내림차순)을 사용했다. (Ascending(오름차순))
+        // UI 개수만큼 Take하기 위해 _items.Count를 사용했다.
+        // Linq는 지연 실행이기 때문에, UI에 안정적으로 바인딩하기 위해 ToList()를 붙여 즉시 실행해준다.
+        var ranking = scoreDatas.OrderByDescending(data => data.Score).Take(_items.Count).ToList();
+
+        for (int i = 0; i < ranking.Count; i++)
         {
-            ScoreData data = scoreDatas[i];
-            _items[i].Set(data.Nickname, data.Score);
+            _items[i].Set(i+1, ranking[i].Nickname, ranking[i].Score);
+            _items[i].gameObject.SetActive(true);
         }
     }
 }
