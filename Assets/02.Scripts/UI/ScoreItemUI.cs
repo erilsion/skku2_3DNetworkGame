a5f2a549f8ef4662a50194ed858acf9aea36b2a0
erilsion <erilsion@gmail.com>
2026-03-03 10:19:08 +0900

    Feat: 랭킹 UI 관련 클래스 생성

---
+++ Assets/02.Scripts/UI/ScoreItemUI.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreItemUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _nicknameTextUI;
+    [SerializeField] private TextMeshProUGUI _scoreTextUI;
+
+    public void Set(string nickname, string score)
+    {
+        _nicknameTextUI.text = nickname;
+        _scoreTextUI.text = $"{score:N0}";
+    }
+}
---
+++ Assets/02.Scripts/UI/ScoreUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUI : MonoBehaviour
+{
+    private List<ScoreItemUI> _items;
+
+    private void Start()
+    {
+        _items = GetComponentsInChildren<ScoreItemUI>().ToList();
+    }
+
+    private void Refresh()
+    {
+
+    }
+}
