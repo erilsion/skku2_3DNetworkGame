e2cbe01ed8a0f5ff4f4f7bcc1dd58c784d8131a7
erilsion <erilsion@gmail.com>
2026-03-03 12:36:25 +0900

    Fix: 곰 공격 시 플레이어에게 데미지가 들어가지 않는 버그 수정

--- Assets/02.Scripts/Monster/Bear/BearController.cs
+++ Assets/02.Scripts/Monster/Bear/BearController.cs
@@ -223,4 +223,17 @@ public class BearController : MonoBehaviourPunCallbacks
             attackState.HandleAttackEnd();
         }
     }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+        if (other.transform == transform) return;
+
+        if (other.TryGetComponent<IDamageable>(out var damageable))
+        {
+            int actorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
+            PlayerController otherPlayer = other.GetComponent<PlayerController>();
+            otherPlayer.PhotonView.RPC(nameof(damageable.TakeDamage), RpcTarget.All, otherPlayer.Stat.Damage, actorNumber);
+        }
+    }
 }
--- Assets/02.Scripts/Monster/Bear/States/BearAttackState.cs
+++ Assets/02.Scripts/Monster/Bear/States/BearAttackState.cs
@@ -45,17 +45,4 @@ public class BearAttackState : BearState
     {
         _bear.ChangeState(EBearStateType.AttackWait);
     }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        if (!PhotonNetwork.IsMasterClient) return;
-        if (other.transform == _bear.transform) return;
-
-        if (other.TryGetComponent<IDamageable>(out var damageable))
-        {
-            int actorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
-            PlayerController otherPlayer = other.GetComponent<PlayerController>();
-            otherPlayer.PhotonView.RPC(nameof(damageable.TakeDamage), RpcTarget.All, otherPlayer.Stat.Damage, actorNumber);
-        }
-    }
 }
